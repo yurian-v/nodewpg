5a360a1bdd9e6d782219fd52fea953e3cb89e8f7
YURIAN <yurian.canada@gmail.com>
2019-09-10 23:05:06 -0500

    started REACT

---
+++ react/ex1/like_button.js
@@ -0,0 +1,15 @@
+'use strict';
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Nico" />,
+  document.getElementById('hello-example')
+);
